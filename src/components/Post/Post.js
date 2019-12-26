d3c08ad5b53cf2154d2384bc7962430852b53fcf
Erdinc <Erdinc@gklfmg.com>
2019-12-26 19:42:19 +0100

    withRouter

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,8 +1,14 @@
 import React from "react";
-
+import { withRouter } from "react-router-dom";
+// withRouter is a higher order component(hoc) which we use by wraping
+// our export with it. withRouter(Post)
+// adds these props to other components
+// this is one way for you to get access to routing related props which
+// you might need for calling push on the history prop or for getting
+// information about the loaded route.
 import "./Post.css";
 
-const post = props => (
+const Post = props => (
   <article className="Post" onClick={props.clicked}>
     <h1>{props.title}</h1>
     <div className="Info">
@@ -11,4 +17,4 @@ const post = props => (
   </article>
 );
 
-export default post;
+export default withRouter(Post);
