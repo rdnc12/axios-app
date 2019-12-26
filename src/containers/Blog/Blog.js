0da09c86c8669b9d37a15bad9d1c32e34585e843
Erdinc <Erdinc@gklfmg.com>
2019-12-26 16:22:51 +0100

    adding navbar to switch without reloading page

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,69 +1,36 @@
 import React, { Component } from "react";
-import axios from "../../axios";
+import Posts from "./Posts/Posts";
+import NewPost from '../Blog/NewPost/NewPost'
 
-
+import { Route,Link } from "react-router-dom";
+// Link prevent page reloading when we click it.
 import "./Blog.css";
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false
-  };
-
-  componentDidMount() {
-    // axios dont get data immediatly. it takes some time to reach database
-    // thats why we use promises.
-    axios
-      .get("/posts")
-      .then(response => {
-        const posts = response.data.slice(0, 4);
-        const updatedPosts = posts.map(post => {
-          return {
-            ...post,
-            author: "Erdinç"
-          };
-        });
-        this.setState({ posts: updatedPosts });
-        // console.log(response);
-      })
-      .catch(error => {
-        this.setState({ error: true });
-        //console.log(error);
-      });
-  }
-
-  postSelectedHandler = id => {
-    this.setState({ selectedPostId: id });
-  };
-
   render() {
-      // error handling
-    let posts = <p style={{ textAlign: "center" }}>Something went wrong!!!</p>;
-    if (!this.state.error) {
-      posts = this.state.posts.map(post => {
-        return (
-          <Post
-            key={post.id}
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}
-          />
-        );
-      });
-    }
     return (
       <div className="Blog">
         <header>
           <nav>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/new-post">New Post</a></li>
+              <li>
+                <Link to='/'> Home</Link>
+              </li>
+              <li>
+                <Link to={{
+                  pathname:'/new-post',
+                  hash:'#submit',
+                  search:'?quick-submit=true'
+                }}>New Post</Link>
+              </li>
             </ul>
           </nav>
         </header>
-        <section className="Posts">{posts}</section>
-        
+        {/* <Route path='/' exact render={()=> <h1>eee</h1>}/> // exact show us only one rendered path
+        <Route path='/'  render={()=> <h1>eee2</h1>}/> */}
+         {/* render is for short info messages. */}
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
       </div>
     );
   }
